dea56be824f630c1b86208652cba73963b264e4b
harshinikalakodimi16-hub <harshinikalakodimi16@gmail.com>
2026-07-11 22:22:41 +0530

    Fix syntax error in fetch request

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ async function askQuestion() {
     try {
         const response = await fetch(
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
-    {
             {
                 method: "POST",
                 headers: {
